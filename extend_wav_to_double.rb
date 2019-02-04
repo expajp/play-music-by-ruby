bed7e4b39d8dbda1367231be94ffc33ab9641ea3
Shu OGAWARA <shu.ogawara@linkers.net>
2019-02-04 13:51:35 +0900

    remove garbages

--- extend_wav_to_double.rb
+++ extend_wav_to_double.rb
@@ -11,14 +11,14 @@ format = WavFile::readFormat(f)
 chunks = WavFile::readDataChunk(f)
 f.close
 
-# format.hz *= 2
-format.bytePerSec *= 2
-
-bit = 's*' # 16 bit PCM
+bit = 's*'
 wavs = chunks.data.unpack(bit)
-extended_wavs = Array.new(wavs.length*2)
-extended_wavs.map!.with_index{ |n, i| wavs[i/2] }
-chunks.data = extended_wavs.pack(bit)
+puts wavs.length
+
+new_wavs = Array.new(wavs.length*2).map.with_index{ |data, index| wavs[index/2] }
+puts new_wavs.length
+
+chunks.data = new_wavs.pack(bit)
 
 open(ARGV.shift, "w"){|out|
   WavFile::write(out, format, [chunks])
